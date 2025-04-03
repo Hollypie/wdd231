9c9275f624f614a6414c0d3c8e7d6ee89b4e75cf
Hollypie <hbriggs@byupathway.edu>
2025-04-02 18:02:31 -0600

    added content to the thankyou page

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -4,9 +4,13 @@ function getQueryParam(param) {
     return params.get(param) || "Not provided";
 }
 
-document.getElementById("firstName").textContent = getQueryParam("firstName");
-document.getElementById("lastName").textContent = getQueryParam("lastName");
+document.getElementById("firstName").textContent = getQueryParam("first");
+document.getElementById("lastName").textContent = getQueryParam("last");
 document.getElementById("email").textContent = getQueryParam("email");
 document.getElementById("phone").textContent = getQueryParam("phone");
 document.getElementById("business").textContent = getQueryParam("organization");
-document.getElementById("timestamp").textContent = getQueryParam("timestamp");
+document.getElementById("submissionTime").textContent = getQueryParam("timestamp");
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("timestamp").value = new Date().toISOString();
+});
